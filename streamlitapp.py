866479eab6a086bcc7a7ba16488d77cb28134d51
alexanderp99 <alexanderp99@unet.univie.ac.at>
2024-03-22 14:36:30 +0100

    skeleton: add vector file storage

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,14 +3,26 @@ import ollama
 
 st.title("Bachelor RAG local LLM")
 
+uploaded_documents = []
+
 uploaded_file = st.file_uploader("Upload a file")
+
+if uploaded_file is not None:
+    uploaded_documents.append(uploaded_file)
+
+if uploaded_documents:
+    st.write("Uploaded Documents:")
+    for document in uploaded_documents:
+        st.write(document)
 
 def respond_with_llm(user_input):
     if "conversation_memory" not in st.session_state:
         st.session_state.conversation_memory = []
 
     st.session_state.conversation_memory.append({'role': 'user', 'content': user_input})
+
     response = ollama.chat(model='gemma:2b', messages=st.session_state.conversation_memory)
+
     st.session_state.conversation_memory.append({'role': 'assistant', 'content': response['message']['content']})
 
     return response['message']['content']
